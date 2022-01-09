5af0a21ac112fe46b06f2a1302d0e86ef47238e2
Connor Hollis <connorhollis3d@gmail.com>
2022-01-09 15:25:00 -0700

    seeded employee data

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,14 @@ INSERT INTO role(title, salary, department_id) VALUES
     ('Accountant', 125000, 3),
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES
+    ('James', 'Fraser', 1, NULL),
+    ('Jack', 'London', 1, 1),
+    ('Robert', 'Bruce', 2, NULL),
+    ('Peter', 'Greenaway', 2, 3),
+    ('Derek', 'Jarman', 3, NULL),
+    ('Paolo', 'Pasolini', 3, 5),
+    ('Heathcote', 'Williams', 4, NULL),
+    ('Sandy', 'Powell', 4, 7)
+    ;
