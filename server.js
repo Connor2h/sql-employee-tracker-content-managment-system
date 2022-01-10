c4f9a9d0bb79fd7080c0dbfcc49c0415d4d4b168
Connor Hollis <connorhollis3d@gmail.com>
2022-01-09 18:15:42 -0700

    updated the server to connect to and added util folder to break up functions in/ also completed department request info

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require('./db/connection');
 const inquirer = require("inquirer");
+const { promptUser, viewAllDepartments } = require('./utils/functions');
 
 // Start server after DB connection
 db.connect(err => {
@@ -7,39 +8,7 @@ db.connect(err => {
     promptUser();
 });
 
-const promptUser = () => {
-    console.log('Database connected.');
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'choice',
-            message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Leave the Database']
-        }
-    ]).then((options) =>{
 
-        if (options.choice === 'View all departments'){
-            console.log('View all departments is true');
-        }
-        else if (options.choice === 'View all roles'){
-            console.log('View all roles is true');
-        }
-        else if (options.choice === 'View all employees'){
-            console.log('View all employees is true');
-        }
-        else if (options.choice === 'Add a department'){
-            console.log('Add a department is true');
-        }
-        else if (options.choice === 'Add a role'){
-            console.log('Add a role is true');
-        }
-        else if (options.choice === 'Add an employee'){
-            console.log('Add an employee is true');
-        }
-        else if (options.choice === 'Update an employee role'){
-            console.log('Update an employee role is true');
-        } else {
-            console.log('Leave the Database is true');
-        }
-    })
-}
+
+
+
---
+++ utils/functions.js
@@ -0,0 +1,55 @@
+const db = require('../db/connection')
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const promptUser = () => {
+    console.log('Database connected.');
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Leave the Database']
+        }
+    ]).then((options) =>{
+
+        if (options.choice === 'View all departments'){
+
+            viewAllDepartments();
+        }
+        else if (options.choice === 'View all roles'){
+            console.log('View all roles is true');
+        }
+        else if (options.choice === 'View all employees'){
+            console.log('View all employees is true');
+        }
+        else if (options.choice === 'Add a department'){
+            console.log('Add a department is true');
+        }
+        else if (options.choice === 'Add a role'){
+            console.log('Add a role is true');
+        }
+        else if (options.choice === 'Add an employee'){
+            console.log('Add an employee is true');
+        }
+        else if (options.choice === 'Update an employee role'){
+            console.log('Update an employee role is true');
+        } else {
+            console.log('Leave the Database is true');
+        }
+    })
+}
+
+function viewAllDepartments (){
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table('\n',rows, '\n');
+        promptUser();// recursion to prompt the main question again
+    });
+
+
+}
+
+module.exports = {promptUser, viewAllDepartments};
