3a8d93bd9e20f80d6a591f441ce5858609a2b3b4
Connor Hollis <connorhollis3d@gmail.com>
2022-01-09 17:16:56 -0700

    added promptUser function to inquirer the user for what choice they would like to do

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const db = require('./db/connection');
+const inquirer = require("inquirer");
 
 // Start server after DB connection
 db.connect(err => {
     if (err) throw err;
-    console.log('Database connected.');
+    promptUser();
 });
+
+const promptUser = () => {
+    console.log('Database connected.');
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Leave the Database']
+        }
+    ]).then((options) =>{
+
+        if (options.choice === 'View all departments'){
+            console.log('View all departments is true');
+        }
+        else if (options.choice === 'View all roles'){
+            console.log('View all roles is true');
+        }
+        else if (options.choice === 'View all employees'){
+            console.log('View all employees is true');
+        }
+        else if (options.choice === 'Add a department'){
+            console.log('Add a department is true');
+        }
+        else if (options.choice === 'Add a role'){
+            console.log('Add a role is true');
+        }
+        else if (options.choice === 'Add an employee'){
+            console.log('Add an employee is true');
+        }
+        else if (options.choice === 'Update an employee role'){
+            console.log('Update an employee role is true');
+        } else {
+            console.log('Leave the Database is true');
+        }
+    })
+}
